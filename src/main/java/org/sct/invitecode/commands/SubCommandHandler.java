ab4e32afd9f1c1b163e4218e7adc9f7250807153
LovesAsuna <qq625924077@gmail.com>
2020-04-07 19:13:57 +0800

    [*] 忘了注册命令

--- src/main/java/org/sct/invitecode/commands/SubCommandHandler.java
+++ src/main/java/org/sct/invitecode/commands/SubCommandHandler.java
@@ -1,6 +1,7 @@
 package org.sct.invitecode.commands;
 
 import org.bukkit.plugin.java.JavaPlugin;
+import org.sct.invitecode.commands.sub.*;
 import org.sct.plugincore.util.function.command.CommandHandler;
 
 /**
@@ -11,6 +12,17 @@ import org.sct.plugincore.util.function.command.CommandHandler;
 public class SubCommandHandler extends CommandHandler {
     public SubCommandHandler(JavaPlugin instance, String cmd) {
         super(instance, cmd);
+        registerSubCommand("check", new Check());
+        registerSubCommand("create", new Create());
+        registerSubCommand("help", new Help());
+        registerSubCommand("info", new Info());
+        registerSubCommand("reload", new Reload());
+        registerSubCommand("reset", new Reset());
+        registerSubCommand("Reward", new Reward());
+        registerSubCommand("saveitem", new SaveItem());
+        registerSubCommand("show", new Show());
+        registerSubCommand("storge", new Storge());
+        registerSubCommand("update", new Update());
     }
 
 }
